977a8656526bb7fb4a914f2547d3f0543d04aa41
dragonfly161518 <por.nattapat.01@gmail.com>
2020-06-19 01:52:38 +0700

    init github pr

---
+++ github-rp-api.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="online">Onlines</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      online: true
+    };
+  },
+  async mounted() {
+    const res = await fetch(
+      "https://api.coindesk.com/v1/bpi/currentprice.json"
+    );
+    const data = await res.json();
+    console.log(data);
+  }
+};
+</script>
