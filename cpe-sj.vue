349bfe38a37446060d1eb15e72e278822e913ea2
dragonfly161518 <por.nattapat.01@gmail.com>
2020-07-19 00:59:27 +0700

    add tailwind

---
+++ cpe-sj.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    HL
+  </div>
+</template>
+
+<script>
+import inf from "./inf.json";
+
+console.log(inf);
+
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
